78112aa5b26f949db5503db6d1b21bd557513647
pjkm00 <pjkm00@gmail.com>
2023-05-28 22:13:28 +0900

    .

--- middle/WebContent/WEB-INF/view/student/findStudent.jsp
+++ middle/WebContent/WEB-INF/view/student/findStudent.jsp
@@ -1,3 +1,4 @@
+<%@page import="kr.or.dw.util.SendEmail"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -8,10 +9,13 @@
 <script src="<%=request.getContextPath()%>/assets/plugins/jquery/jquery.js"></script>
 <script>
 	$(function(){
+		let ver_code = null;
+		
 		$('#idEmailChkBtn').on('click', function(){
 			let stu_id = $('#stu_id').val();
 			let stu_email = $('#stu_email').val();
-			
+			console.log(stu_id);
+			console.log(stu_email);
 			$.ajax({
 				url : "<%=request.getContextPath()%>/student/idEmailCheck.do",
 				method : "post",
@@ -21,12 +25,28 @@
 					"stu_email" : stu_email
 				},
 				success : function(res){
-					alert(res);
+					if(res != null){
+						console.log(res);
+						ver_code = res;
+						alert("인증코드가 전송되었습니다.");
+						$('#ver_code').css("display", "");
+						$('#verCodeBtn').css("display", "");
+					}else{
+						alert("아이디 혹은 이메일이 일치하지 않습니다.");
+					}
 				},
 				error : function(err){
-					alert(err.staus);
+					
 				}
 			});
+		});
+		
+		$(document).on('click', '#verCodeBtn', function(e) {
+			if($('#ver_code').val() == ver_code){
+			location.href="<%=request.getContextPath()%>/student/passwordUpdate.do";
+		}else{
+			alert("인증코드가 잘못입력되었습니다.");
+		}
 		});
 	})
 </script>
@@ -41,6 +61,10 @@
 		이메일 <input id="stu_email" name="stu_email">
 		
 		<input type="button" id="idEmailChkBtn" value="인증코드 전송">
+		<br>
+		<br>
+		<input type="text" id="ver_code" name="ver_code" placeholder="인증코드 입력" style="display: none">
+		<input type="button" id="verCodeBtn" value="인증코드 확인" style="display: none">
 	</form>
 </body>
 </html>
--- middle/src/kr/or/dw/util/SendEmail.java
+++ middle/src/kr/or/dw/util/SendEmail.java
@@ -1,7 +1,8 @@
 package kr.or.dw.util;
  
 import java.util.Properties;
- 
+import java.util.Random;
+
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
@@ -10,8 +11,33 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
  
-public class Email {
-	public static void main(String[] args) {
+public class SendEmail {
+	
+	public String pwdEmail(String stu_email) {
+		int result = 0;
+		
+		StringBuffer temp = new StringBuffer();
+		Random rnd = new Random();
+		for (int i = 0; i < 10; i++) {
+			int rIndex = rnd.nextInt(3);
+			switch (rIndex) {
+			case 0:
+				// a-z
+				temp.append((char) ((int) (rnd.nextInt(26)) + 97));
+				break;
+			case 1:
+				// A-Z
+				temp.append((char) ((int) (rnd.nextInt(26)) + 65));
+				break;
+			case 2:
+				// 0-9
+				temp.append((rnd.nextInt(10)));
+				break;
+			}
+		}
+		String AuthenticationKey = temp.toString();
+		System.out.println(AuthenticationKey);
+		
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
@@ -27,9 +53,9 @@ public class Email {
 			}
 		});
 		
-		String receiver = "pjkm00@gmail.com"; // 메일 받을 주소
+		String receiver = stu_email; // 메일 받을 주소
 		String title = "비밀번호 인증 코드입니다.";
-		String content = "<h2 style='color:blue'>안녕하세요</h2>";
+		String content = "인증번호는 " + AuthenticationKey + "입니다.";
 		Message message = new MimeMessage(session);
 		try {
 			message.setFrom(new InternetAddress("dwuniversity4@gmail.com", "DW대학교", "utf-8"));
@@ -38,8 +64,11 @@ public class Email {
 			message.setContent(content, "text/html; charset=utf-8");
  
 			Transport.send(message);
+			System.out.println("메일전송 성공");
+			result = 1;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return AuthenticationKey;
 	}
 }
