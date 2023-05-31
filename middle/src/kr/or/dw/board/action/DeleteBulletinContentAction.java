e777453368b9d7858d336afb9d83351f86ab4a27
pjkm00 <pjkm00@gmail.com>
2023-05-31 15:58:48 +0900

    .

--- middle/src/kr/or/dw/board/action/DeleteBulletinContentAction.java
+++ middle/src/kr/or/dw/board/action/DeleteBulletinContentAction.java
@@ -1,5 +1,26 @@
 package kr.or.dw.board.action;
 
-public class DeleteBulletinContentAction {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.dw.web.IAction;
+
+public class DeleteBulletinContentAction implements IAction{
+
+	@Override
+	public boolean isRedirect() {
+		return true;
+	}
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		
+		
+		return "/board/bulletinBoardList.do";
+	}
 
 }
