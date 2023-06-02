8eb895e513f701af4d6da32c13e63f5b94819017
choi-DongJu <ehdehdwn131@gmail.com>
2023-06-02 12:40:57 +0900

    dj

--- middle/src/kr/or/dw/board/action/StuBoardReplyInsertAction.java
+++ middle/src/kr/or/dw/board/action/StuBoardReplyInsertAction.java
@@ -1,5 +1,41 @@
 package kr.or.dw.board.action;
 
-public class StuBoardReplyInsertAction {
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import kr.or.dw.board.service.BoardServiceImpl;
+import kr.or.dw.board.service.IBoardService;
+import kr.or.dw.vo.ReplyVO;
+import kr.or.dw.web.IAction;
+
+public class StuBoardReplyInsertAction implements IAction{
+
+	@Override
+	public boolean isRedirect() {
+		return true;
+	}
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		IBoardService boardService = BoardServiceImpl.getInstance();
+		ReplyVO reVo = new ReplyVO();
+		BeanUtils bean = new BeanUtils();
+		
+		try {
+			bean.populate(reVo, req.getParameterMap());
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 
 }
